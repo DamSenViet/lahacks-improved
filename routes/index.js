1a0602eec8d426a38104a9b8a1995b07af7d20f4
Viet Tran <vietdaitran1998@gmail.com>
2018-03-31 02:46:28 -0700

    templating pages

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
     res.render('index', null);
     console.log(req.body);
-})
+});
 
 
 module.exports = router;
