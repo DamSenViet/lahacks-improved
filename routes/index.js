e83ceb239008d5517536bbb696b5b4f7ad985f9c
Viet Tran <vietdaitran1998@gmail.com>
2018-04-18 03:27:25 -0700

    fixed IE11 issues

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,40 @@ router.get('/', function(req, res, next) {
     res.render('upload');
 });
 
-router.post('/TEMPORARY', function(req, res, next) {
+router.get('/login/', function(req, res, next) {
+
+});
+
+
+router.post('/login/', function(req, res, next) {
+
+});
+
+
+router.get('/signup/', function(req, res, next) {
+
+});
+
+
+router.post('/signup/', function(req, res, next) {
+
+});
+
+
+router.get('/upload/*', function(req, res, next) {
+
+});
+
+router.post('/upload/*', function(req, res, next) {
+
+});
+
+router.post('/TEMPORARY/', function(req, res, next) {
     console.log(req.body.title);
     console.log(req.body.description);
     // console.log(req.body.imgData);
-    res.send({message: "success"});
+    res.status(400);
+    res.send({title:'too long', file: 'oops'});
 });
 
 module.exports = router;
